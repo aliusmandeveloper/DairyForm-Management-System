2b7195ca13714576a176212e320ea5ad1ae78cd1
Muhammad Ali Usman <aliusmanijaz143@example.com>
2025-10-08 15:44:05 +0500

    dashbaord is set on role base

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Sidebarr from './components/sidebar/Sidebarr';
 
-function layout({ children }) {
-    return (
-        <div>
-            <div className='bg-gray-100'>
-                {children}
-            </div>
-        </div>
-    )
+export default function Layout({ children }) {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
+      <aside className="w-64 bg-white shadow-md">
+        <Sidebarr />
+      </aside>
+
+      {/* Main content */}
+      <main className="flex-1 p-6">{children}</main>
+    </div>
+  );
 }
-
-export default layout
