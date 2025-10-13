3e9032c0196a96e3cdde06950d2520fbbe19b6bd
Muhammad Ali Usman <aliusmanijaz143@example.com>
2025-10-13 20:51:47 +0500

    solve build errror

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
+import { Suspense } from "react";
 import ResetPassword from "../../components/ResetPassword";
 
 export default function ResetPasswordPage() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <div className="w-full flex mt-20 justify-center">
         <section className="flex flex-col w-[400px]">
           <h1 className="text-3xl w-full text-center font-bold mb-6">
@@ -11,6 +14,6 @@ export default function ResetPasswordPage() {
           <ResetPassword />
         </section>
       </div>
-    </>
+    </Suspense>
   );
 }
